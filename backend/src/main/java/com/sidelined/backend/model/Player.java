4ef601566f189c61c1f715437bc646e99de48a28
Justin <jgreen38@tulane.edu>
2026-09-17 13:13:30 -0500

    DEV-50: Hardcode all the starting offensive players for each team, and add a position attribute to the player objects.

--- backend/src/main/java/com/sidelined/backend/model/Player.java
+++ backend/src/main/java/com/sidelined/backend/model/Player.java
@@ -5,13 +5,15 @@ public class Player {
     private Long id;
     private String name;
     private String team;
+    private String position;
     private int snapsLastGame;
     private int snapsLast4Games;
 
-    public Player(Long id, String name, String team, int snapsLastGame, int snapsLast4Games) {
+    public Player(Long id, String name, String team, String position, int snapsLastGame, int snapsLast4Games) {
         this.id = id;
         this.name = name;
         this.team = team;
+        this.position = position;
         this.snapsLastGame = snapsLastGame;
         this.snapsLast4Games = snapsLast4Games;
     }
@@ -19,6 +21,7 @@ public class Player {
     public Long getId()           { return id; }
     public String getName()       { return name; }
     public String getTeam()       { return team; }
+    public String getPosition()     { return position; }
     public int getSnapsLastGame() { return snapsLastGame; }
     public int getSnapsLast4Games() { return snapsLast4Games; }
 }
